2577493b3c38c4d757ef668d34c73f1881fb0b77
manuelnunziata <nunziatamanuel5@gmail.com>
2026-08-31 11:36:42 +0200

    BW4

---
+++ src/main/java/manuelnunziata/buildweek4/BuildWeek4Application.java
@@ -0,0 +1,13 @@
+package manuelnunziata.buildweek4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWeek4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuildWeek4Application.class, args);
+    }
+
+}
---
+++ src/test/java/manuelnunziata/buildweek4/BuildWeek4ApplicationTests.java
@@ -0,0 +1,13 @@
+package manuelnunziata.buildweek4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildWeek4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
