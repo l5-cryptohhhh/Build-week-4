1dec554951522d403b078c2492954b9d19c5e5f0
simone pierantozzi <pierantozzisimone@gmail.com>
2026-09-02 10:14:06 +0200

    repository indirizzo

---
+++ src/main/java/manuelnunziata/buildweek4/repositories/IndirizzoRepository.java
@@ -0,0 +1,7 @@
+package manuelnunziata.buildweek4.repositories;
+
+import manuelnunziata.buildweek4.entities.Indirizzo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IndirizzoRepository extends JpaRepository<Indirizzo, Long> {
+}
