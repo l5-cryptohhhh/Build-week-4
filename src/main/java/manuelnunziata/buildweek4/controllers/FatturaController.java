ba8fa009559f2c10835d815d00ef5421301b732c
Marco C. <marcocozma97@gmail.com>
2026-09-02 16:12:37 +0200

    Aggiornamento Fattura
    Aggiunta metodi in fattura service, aggiornamento endpoint fattura controller

---
+++ src/main/java/manuelnunziata/buildweek4/controllers/FatturaController.java
@@ -0,0 +1,41 @@
+package manuelnunziata.buildweek4.controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import manuelnunziata.buildweek4.entities.Fattura;
+import manuelnunziata.buildweek4.entities.StatoFattura;
+import manuelnunziata.buildweek4.entities.Utenti;
+import manuelnunziata.buildweek4.payloads.FatturaDTO;
+import manuelnunziata.buildweek4.services.FatturaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fatture")
+@RequiredArgsConstructor
+@PreAuthorize("hasAnyRole('ADMIN', 'COMMERCIALE', 'CONTABILE')")
+
+public class FatturaController {
+    private final FatturaService fatturaService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Fattura create(@RequestBody @Valid FatturaDTO dto, @AuthenticationPrincipal Utenti richiedente) {
+        return fatturaService.create(dto, richiedente);
+    }
+
+    @GetMapping
+    public List<Fattura> findAll(@RequestParam(required = false) Long clienteId,
+                                 @RequestParam(required = false) StatoFattura stato,
+                                 @AuthenticationPrincipal Utenti richiedente) {
+        return fatturaService.findAll(richiedente, clienteId, stato);
+    }
+
+    @GetMapping("/{id}")
+    public Fattura findById(@PathVariable Long id, @AuthenticationPrincipal Utenti richiedente) {
+        return fatturaService.findById(id, richiedente);
+    }
+}
---
+++ src/main/java/manuelnunziata/buildweek4/repositories/FatturaRepository.java
@@ -0,0 +1,14 @@
+package manuelnunziata.buildweek4.repositories;
+
+
+import manuelnunziata.buildweek4.entities.Fattura;
+import manuelnunziata.buildweek4.entities.StatoFattura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FatturaRepository extends JpaRepository<Fattura, Long> {
+    boolean existsByClienteId(Long clienteId);
+    List<Fattura> findByClienteId(Long clienteId);
+    List<Fattura> findByStato(StatoFattura stato);
+}
