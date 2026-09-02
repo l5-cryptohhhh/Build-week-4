dfbfd53537187726971ec504cc1684b404f622b4
simone pierantozzi <pierantozzisimone@gmail.com>
2026-09-02 14:22:20 +0200

    aggiustamento
    aggiunto elsethrow in clienteservice

--- src/main/java/manuelnunziata/buildweek4/services/ClienteService.java
+++ src/main/java/manuelnunziata/buildweek4/services/ClienteService.java
@@ -6,9 +6,11 @@ import manuelnunziata.buildweek4.entities.Cliente;
 import manuelnunziata.buildweek4.entities.Indirizzo;
 import manuelnunziata.buildweek4.entities.Ruolo;
 import manuelnunziata.buildweek4.entities.Utenti;
+import manuelnunziata.buildweek4.exceptions.NotFoundException;
 import manuelnunziata.buildweek4.exceptions.UnauthorizedException;
 import manuelnunziata.buildweek4.payloads.ClienteDTO;
 import manuelnunziata.buildweek4.repositories.ClienteRepository;
+import manuelnunziata.buildweek4.repositories.IndirizzoRepository;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,13 +18,14 @@ import org.springframework.stereotype.Service;
 
 public class ClienteService {
     private final ClienteRepository clienteRepository;
-    //private final IndirizzoRepository indirizzoRepository;
+    private final IndirizzoRepository indirizzoRepository;
     //private final UtentiRepository utentiRepository;
     //private final FatturaRepository fatturaRepository;
     //private final NotaRepository notaRepository;
 
     public Cliente creazione(ClienteDTO dto, Utenti richiedente) {
-        Indirizzo indirizzo = indirizzoRepository.findById(dto.indirizzoId());
+        Indirizzo indirizzo = indirizzoRepository.findById(dto.indirizzoId())
+                .orElseThrow(() -> new NotFoundException("Indirizzo con id " + dto.indirizzoId() + " non trovato"));
         Utenti commerciale = resolveCommerciale(dto, richiedente);
         Cliente cliente = new Cliente();
         applyDto(cliente, dto, indirizzo);
