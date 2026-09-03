b498e8fe5c560083affdd94a70ea181cf4338054
gianlucaciliberti <gianlu.cili.gc@gmail.com>
2026-09-03 11:46:11 +0200

    JwtTools e JwtFilter

--- src/main/java/manuelnunziata/buildweek4/security/JwtFilter.java
+++ src/main/java/manuelnunziata/buildweek4/security/JwtFilter.java
@@ -1,4 +1,48 @@
 package manuelnunziata.buildweek4.security;
 
-public class JwtFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import manuelnunziata.buildweek4.entities.Utenti;
+import manuelnunziata.buildweek4.repositories.UtentiRepository;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtTools jwtTools;
+    private final UtentiRepository utentiRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            String token = authHeader.substring(7);
+            String userId = jwtTools.validateToken(token);
+            Utenti utente = utentiRepository.findById(Long.valueOf(userId)).orElse(null);
+
+            if (utente != null) {
+                var authentication = new UsernamePasswordAuthenticationToken(
+                        utente, null, utente.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) {
+        return request.getServletPath().startsWith("/auth");
+    }
 }
