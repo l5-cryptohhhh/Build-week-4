ca971f3a43e9e3de66c4107f4246ef795c8f0d01
Marco C. <marcocozma97@gmail.com>
2026-09-03 15:52:08 +0200

    Aggiornamento fattura
    Conclusione fattura e modifiche.

--- src/main/java/manuelnunziata/buildweek4/controllers/FatturaController.java
+++ src/main/java/manuelnunziata/buildweek4/controllers/FatturaController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import manuelnunziata.buildweek4.entities.Fattura;
 import manuelnunziata.buildweek4.entities.StatoFattura;
 import manuelnunziata.buildweek4.entities.Utenti;
+import manuelnunziata.buildweek4.payloads.CambioStatoFatturaDTO;
 import manuelnunziata.buildweek4.payloads.FatturaDTO;
 import manuelnunziata.buildweek4.services.FatturaService;
 import org.springframework.http.HttpStatus;
@@ -18,9 +19,10 @@ import java.util.List;
 @RequestMapping("/fatture")
 @RequiredArgsConstructor
 @PreAuthorize("hasAnyRole('ADMIN', 'COMMERCIALE', 'CONTABILE')")
-
 public class FatturaController {
+
     private final FatturaService fatturaService;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Fattura create(@RequestBody @Valid FatturaDTO dto, @AuthenticationPrincipal Utenti richiedente) {
@@ -37,5 +39,22 @@ public class FatturaController {
     @GetMapping("/{id}")
     public Fattura findById(@PathVariable Long id, @AuthenticationPrincipal Utenti richiedente) {
         return fatturaService.findById(id, richiedente);
+    }
+
+    @PutMapping("/{id}")
+    public Fattura update(@PathVariable Long id, @RequestBody @Valid FatturaDTO dto, @AuthenticationPrincipal Utenti richiedente) {
+        return fatturaService.update(id, dto, richiedente);
+    }
+
+    @PatchMapping("/{id}/stato")
+    @PreAuthorize("hasAnyRole('ADMIN', 'CONTABILE')")
+    public Fattura cambiaStato(@PathVariable Long id, @RequestBody @Valid CambioStatoFatturaDTO dto, @AuthenticationPrincipal Utenti richiedente) {
+        return fatturaService.cambiaStato(id, dto.stato(), richiedente);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id, @AuthenticationPrincipal Utenti richiedente) {
+        fatturaService.delete(id, richiedente);
     }
 }
